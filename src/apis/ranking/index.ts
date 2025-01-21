399aa6fe9d290b54b4a03f54d322245382be4c45
gahyeon <gus_05@naver.com>
2025-01-21 19:45:31 +0900

    fix: 인기스테이 불러오기 로직 수정

--- src/apis/ranking/index.ts
+++ src/apis/ranking/index.ts
@@ -1,13 +1,11 @@
-// query 로직
-
 import getRanking from '@apis/ranking/axios';
 import { RankingResponse } from '@apis/ranking/type';
 import { useQuery } from '@tanstack/react-query';
 
-const useGetRanking = () => {
-  const { data, isLoading, isError } = useQuery<RankingResponse[]>({
-    queryKey: [],
-    queryFn: () => getRanking(),
+const useGetRanking = (userId?: number) => {
+  const { data, isLoading, isError } = useQuery<RankingResponse>({
+    queryKey: ['rankiing', userId],
+    queryFn: () => getRanking(userId),
   });
 
   return { data, isLoading, isError };
