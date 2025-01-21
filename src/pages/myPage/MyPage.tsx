cabc543ccd18cebcd847529c699406c3abf575cc
Taew00k <davis0625@dgu.ac.kr>
2025-01-22 05:44:19 +0900

    faet: 마이페이지 가운데 정렬

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -2,9 +2,11 @@ import PageName from '@components/common/pageName/PageName';
 import Footer from '@components/footer/Footer';
 import UserInfo from '@components/userInfo/userInfo';
 
+import myPageWrapper from './myPage.css';
+
 const MyPage = () => {
   return (
-    <div>
+    <div className={myPageWrapper}>
       <PageName title="마이페이지" isLikeBtn={false} />
       <UserInfo />
       <Footer />
---
+++ src/pages/myPage/myPage.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+const myPageWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+export default myPageWrapper;
