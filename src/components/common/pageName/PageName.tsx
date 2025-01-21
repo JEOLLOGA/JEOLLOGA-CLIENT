70f7da9d5bc803d4f33194c710f771dbbb850525
Taew00k <davis0625@dgu.ac.kr>
2025-01-21 19:49:20 +0900

    chore: 리베이스 충돌 해결

--- src/components/common/pageName/PageName.tsx
+++ src/components/common/pageName/PageName.tsx
@@ -1,5 +1,5 @@
 import Icon from '@assets/svgs';
-import useNavigateTo from '@hooks/useNavigateTo';
+import { useState } from 'react';
 
 import * as PageNameStyle from './pageName.css';
 
@@ -9,8 +9,12 @@ interface PageNameProps {
   isLikeBtn?: boolean;
 }
 
-const PageName = ({ title, onRightClick, isLikeBtn = true }: PageNameProps) => {
-  const handleToBack = useNavigateTo(-1);
+const PageName = ({ onLeftClick, title, onRightClick, isLikeBtn = true }: PageNameProps) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const handleLikeClick = () => {
+    setIsLiked((prev) => !prev);
+    if (onRightClick) onRightClick();
+  };
 
   return (
     <nav className={PageNameStyle.container}>
@@ -19,8 +23,8 @@ const PageName = ({ title, onRightClick, isLikeBtn = true }: PageNameProps) => {
       </button>
       <span className={PageNameStyle.titleStyle}>{title}</span>
       {isLikeBtn && (
-        <button className={PageNameStyle.buttonLayout} onClick={onRightClick}>
-          <Icon.IcnWish />
+        <button className={PageNameStyle.buttonLayout} onClick={handleLikeClick}>
+          {isLiked ? <Icon.IcnFlowerPink /> : <Icon.IcnFlowerGray />}
         </button>
       )}
     </nav>
