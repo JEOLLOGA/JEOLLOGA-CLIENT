c73f6a15b1178650905b794f917ba09c85d34ff1
gahyeon <gus_05@naver.com>
2025-01-22 05:55:04 +0900

    feat: 카카로 로그인 연결중 ...

---
+++ src/pages/RedirectionPage/RedirectionPage.tsx
@@ -0,0 +1,18 @@
+import useGetKakaoLogin from '@apis/login';
+import React, { useEffect } from 'react';
+
+const RedirectionPage = () => {
+  const code: string = new URL(window.location.href).searchParams.get('code') || '';
+
+  window.history.forward();
+
+  const { mutate } = useGetKakaoLogin();
+
+  useEffect(() => {
+    if (code) mutate(code);
+  }, [code, mutate]);
+
+  return <div>로띠돌리기~</div>;
+};
+
+export default RedirectionPage;
