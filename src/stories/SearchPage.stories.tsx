5fa98daf1bb815de1a9deb128b685bd529eef075
bykbyk0401 <bykbyk0401@gmail.com>
2025-01-21 15:50:24 +0900

    chore: SearchPage 스토리북 args 추가

--- src/stories/SearchPage.stories.tsx
+++ src/stories/SearchPage.stories.tsx
@@ -8,6 +8,9 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  args: {
+    onSearch: (text: string) => alert(`Search: ${text}`),
+  },
 } satisfies Meta<typeof SearchPage>;
 
 export default meta;
