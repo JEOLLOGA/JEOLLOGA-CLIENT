08f73b50f0fd963983bd473b017cfa6aa5408b26
gahyeon <gus_05@naver.com>
2025-01-22 06:04:56 +0900

    fix: userId 타입 수정

--- src/apis/ranking/axios.ts
+++ src/apis/ranking/axios.ts
@@ -1,6 +1,6 @@
 import { privateInstance } from '@apis/instance';
 
-const getRanking = async (userId: string | null) => {
+const getRanking = async (userId: number | null) => {
   const res = await privateInstance.get('/ranking', {
     params: { userId },
   });
