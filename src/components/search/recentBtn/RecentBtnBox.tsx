8a471ad4e5dc3765b575ceef4ebcbfccbaa5851b
gahyeon <gus_05@naver.com>
2025-01-22 23:02:52 +0900

    feat: 최근 검색어 리스트 반환 api 연결

--- src/components/search/recentBtn/RecentBtnBox.tsx
+++ src/components/search/recentBtn/RecentBtnBox.tsx
@@ -1,13 +1,26 @@
+import { useGetSearchHistory } from '@apis/search';
 import BasicBtn from '@components/common/button/basicBtn/BasicBtn';
 import * as styles from '@components/search/recentBtn/recentBtnBox.css';
 
-const RecentBtnBox = ({ data }: { data: { id: number; content: string }[] }) => {
+const RecentBtnBox = () => {
+  const userId = localStorage.getItem('userId');
+
+  const { data, isLoading, isError } = useGetSearchHistory(Number(userId));
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error</p>;
+  }
+
   return (
     <div className={styles.recentBtnBox}>
-      {data.length > 0 ? (
-        data.map((item) => (
+      {data && data.searchHistory.length > 0 ? (
+        data.searchHistory.map((item) => (
           <BasicBtn
-            key={item.id}
+            key={item.searchId}
             label={item.content}
             variant="lightGrayOutlined"
             size="small"
