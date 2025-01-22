3deb1b951abbcb289fbd6944767b2e1291f3b7f2
bykbyk0401 <bykbyk0401@gmail.com>
2025-01-22 23:26:48 +0900

    feat: 비로그인 접속 페이지 로티 적용

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -3,6 +3,7 @@ import PageName from '@components/common/pageName/PageName';
 import LOGIN_INFOS from '@constants/loginInfos';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import '@dotlottie/player-component';
 
 import * as styles from './loginPage.css';
 
@@ -13,7 +14,7 @@ const LoginPage = () => {
 
   const type: LoginType = location.state?.type || 'my';
 
-  const { title, text, imageSrc, alt } = LOGIN_INFOS[type];
+  const { title, text, lottie } = LOGIN_INFOS[type];
 
   return (
     <section className={styles.loginWrapper}>
@@ -21,7 +22,12 @@ const LoginPage = () => {
       <div className={styles.contentWrapper}>
         <h2 className={styles.textStyle}>{text}</h2>
 
-        <img src={imageSrc} alt={alt} className={styles.imgStyle} width={270} />
+        <dotlottie-player
+          src={lottie}
+          autoplay
+          loop
+          style={{ width: '27rem', height: '28.8rem' }}
+        />
       </div>
       <KakaoBtn />
     </section>
