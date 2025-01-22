15945794d886e4ed9ff68f4dd1e350972886c7e0
moonseonghui <msh9980@naver.com>
2025-01-23 01:40:59 +0900

    feat: 홈페이지 지도 필터링 기능 구현

--- src/components/card/mapCard/Map.tsx
+++ src/components/card/mapCard/Map.tsx
@@ -1,12 +1,28 @@
 import LocBtn from '@components/card/mapCard/LocBtn';
 import mapStyle from '@components/card/mapCard/map.css';
 import REGION_INFOS from '@constants/regionInfos';
+import useFilter from '@hooks/useFilter';
 
 const Map = () => {
+  const { toggleFilter, handleSearch } = useFilter();
+
+  const handleClickMap = async (region: string) => {
+    await toggleFilter(region);
+    handleSearch();
+  };
+
   return (
     <div className={mapStyle}>
       {Object.entries(REGION_INFOS).map(([region, { top, left }]) => (
-        <LocBtn key={region} region={region} top={top} left={left} />
+        <LocBtn
+          key={region}
+          region={region}
+          top={top}
+          left={left}
+          onClick={() => {
+            handleClickMap(region);
+          }}
+        />
       ))}
     </div>
   );
