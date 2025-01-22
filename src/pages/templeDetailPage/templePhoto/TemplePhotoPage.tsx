46910ff33dfaa53918a257b632b7816a36571b01
Taew00k <davis0625@dgu.ac.kr>
2025-01-22 18:33:48 +0900

    feat: params로 템플스테이 id 가져오기 구현

--- src/pages/templeDetailPage/templePhoto/TemplePhotoPage.tsx
+++ src/pages/templeDetailPage/templePhoto/TemplePhotoPage.tsx
@@ -1,18 +1,34 @@
+import useGetTempleImages from '@apis/templeImages';
 import PageName from '@components/common/pageName/PageName';
-import TEMPLE_PHOTO_DATA from '@constants/templePhoto';
+import { useParams } from 'react-router-dom';
 
 import * as styles from './templePhotoPage.css';
 
 const TemplePhotoPage = () => {
+  const { templestayId } = useParams();
+  const { data, isLoading, isError } = useGetTempleImages(String(templestayId));
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error</p>;
+  }
+
+  if (!data) {
+    return <p>No user information available</p>;
+  }
+
   return (
     <div>
       <PageName title="사진" isLikeBtn={false} />
       <div className={styles.photoGrid}>
-        {TEMPLE_PHOTO_DATA.templestayImgs.map((photo) => (
+        {data.templestayImgs.map((photo) => (
           <img
-            key={photo.id}
+            key={photo.imageUrlId}
             src={photo.imgUrl}
-            alt={`Temple Stay ${photo.id}`}
+            alt={`Temple Stay ${photo.imageUrlId}`}
             className={styles.photoItem}
           />
         ))}
