6e8dcbd979d79f6a39cb8e94786c376878f3522a
bykbyk0401 <bykbyk0401@gmail.com>
2025-01-23 17:52:29 +0900

    fix: 온보딩 마지막 페이지 로티 borderRadius 스타일 적용

--- src/pages/welcomePage/WelcomePage.tsx
+++ src/pages/welcomePage/WelcomePage.tsx
@@ -15,12 +15,9 @@ const WelcomePage = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.titleStyle}>{`${userName}${WELCOME_TEXT}`}</h1>
-      <dotlottie-player
-        src="src/assets/lotties/onboarding.lottie"
-        autoplay
-        loop
-        style={{ width: '33.5rem', height: '34.8rem' }}
-      />
+      <div className={styles.lottieStyle}>
+        <dotlottie-player src="src/assets/lotties/onboarding.lottie" autoplay loop />
+      </div>
       <PageBottomBtn btnText="절로가 시작하기" size="large" onClick={handleStart} />
     </div>
   );
