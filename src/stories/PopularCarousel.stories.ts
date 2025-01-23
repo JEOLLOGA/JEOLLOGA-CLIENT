f1a471de4565d7b493499d2d2034c8b59e871e4a
bykbyk0401 <bykbyk0401@gmail.com>
2025-01-24 07:36:05 +0900

    chore: 스토리북 변경사항 반영

--- src/stories/PopularCarousel.stories.ts
+++ src/stories/PopularCarousel.stories.ts
@@ -14,4 +14,8 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    onRequireLogin: () => alert('로그인이 필요합니다.'),
+  },
+};
