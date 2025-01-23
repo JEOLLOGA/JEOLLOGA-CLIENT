9854086b6df977e5f90ea5919c41fd8fd8ee11d4
bykbyk0401 <bykbyk0401@gmail.com>
2025-01-23 20:29:52 +0900

    fix: 홈페이지 위시 아이콘 변경 및 스타일 적용

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,20 +4,21 @@ import useNavigateTo from '@hooks/useNavigateTo';
 import * as styles from './header.css';
 
 const Header = () => {
-  const handleClick = () => {};
   const navigateToWishList = useNavigateTo('/wishList');
   const navigateToMyPage = useNavigateTo('/myPage');
 
   return (
     <header className={styles.headerContainer}>
-      <button onClick={handleClick}>
+      <button onClick={useNavigateTo('/')}>
         <Icon.SmallLogo />
       </button>
       <nav className={styles.iconBox}>
         <Icon.IcnSearchLargeBlack className={styles.iconStyle} onClick={useNavigateTo('/search')} />
-        <Icon.IcnSearchLargeBlack className={styles.iconStyle} onClick={() => handleClick()} />
-        <Icon.IcnWish className={styles.iconStyle} onClick={navigateToWishList} />
         <Icon.IcnMyPage className={styles.iconStyle} onClick={navigateToMyPage} />
+        <Icon.IcnFlowerPink
+          className={`${styles.iconStyle} ${styles.flowerPinkStyle}`}
+          onClick={navigateToWishList}
+        />
       </nav>
     </header>
   );
