3f4e672e46413e50823d96d226bca623366872f8
moonseonghui <msh9980@naver.com>
2025-04-02 00:59:05 +0900

    fix: page폴더에 있던 page파일 삭제

--- src/pages/onboardingPage/onboardingPage.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-const container = style({
-  height: 'calc(var(--vh, 1vh) * 100)',
-  display: 'grid',
-  gridTemplateRows: 'auto 1fr',
-});
-
-export default container;
--- src/pages/searchPage/searchPage.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-const leftPaddingStyle = style({
-  paddingLeft: '2rem',
-});
-
-export default leftPaddingStyle;
