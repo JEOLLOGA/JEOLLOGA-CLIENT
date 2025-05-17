dacae4a3d916a727ec61b0eece3e444ac7efb0c7
gahyeon <gus_05@naver.com>
2025-05-17 23:54:08 +0900

    chore: 주석 삭제

--- src/constants/sort.ts
+++ src/constants/sort.ts
@@ -11,11 +11,3 @@ export const SORT_LABELS: Record<SortOption, string> = {
   [SORT_OPTIONS.LIKE_DESC]: '찜 많은순',
   [SORT_OPTIONS.PRICE_ASC]: '가격 낮은순',
 };
-
-// export const SORT_OPTIONS = [
-//   { label: '추천순', value: 'recommended' },
-//   { label: '찜 많은순', value: 'like_desc' },
-//   { label: '가격 낮은순', value: 'price_asc' },
-// ] as const;
-
-// export type SortOptionValue = (typeof SORT_OPTIONS)[number]['value'];
