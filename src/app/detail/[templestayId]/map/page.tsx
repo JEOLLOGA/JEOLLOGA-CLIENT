69ec3a7a6f18cc9dca522d12b809e382d8eff1fc
Taew00k <davis0625@dgu.ac.kr>
2025-06-24 17:30:02 +0900

    style: 화살표 함수로 변경

--- src/app/detail/[templestayId]/map/page.tsx
+++ src/app/detail/[templestayId]/map/page.tsx
@@ -3,11 +3,11 @@ import MapContainer from '@components/templeDetail/naverMap/MapContainer';
 
 import * as styles from './style.css';
 
-export default async function LargeMap({
+const LargeMap = async ({
   searchParams,
 }: {
   searchParams: { latitude: string; longitude: string };
-}) {
+}) => {
   const params = await searchParams;
   const latitude = params.latitude ? parseFloat(params.latitude) : 0;
   const longitude = params.longitude ? parseFloat(params.longitude) : 0;
@@ -22,4 +22,6 @@ export default async function LargeMap({
       </div>
     </>
   );
-}
+};
+
+export default LargeMap;
